9a8675efe0a8239313e33fc38a0558f341e29007
Saeid <pahangdar@gmail.com>
2024-02-01 18:41:03 -0500

    Add project files.

---
+++ Bookkeepify/Data/Migrations/20240201010117_01-init-database.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookkeepify.Data.Migrations
+{
+    public partial class _01initdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Bookkeepify/Service/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace Bookkeepify.Service
+{
+    public class EmailSender
+    {
+        public bool SendMail(string email, string message1)
+        {
+            MailMessage message = new MailMessage();
+            SmtpClient smtpClient = new SmtpClient();
+            message.From = new MailAddress("yourFrom@email.com");
+            message.To.Add(email);
+            message.Subject = "Confirm Your Email";
+            message.IsBodyHtml = true;
+            message.Body = "<a href='" + message1 + "'>Click here to confirm you email</a>";
+
+            smtpClient.Port = 587;
+            smtpClient.Host = "sandbox.smtp.mailtrap.io";
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential("b7a0fd8832541d", "d8a241e7e43198");
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.Send(message);
+            return true;
+        }
+    }
+}
